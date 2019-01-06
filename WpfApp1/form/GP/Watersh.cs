da6678f95f852d038c8197fe7f19c7a11ea4c970
钟浩伟 <610596547@qq.com>
2019-01-06 17:43:51 +0800

    19-1-6

---
+++ WpfApp1/form/GP/Watersh.cs
@@ -0,0 +1,85 @@
+﻿using Esri.ArcGISRuntime.LocalServices;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Tasks.Geoprocessing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.form.GP
+{
+    /// <summary>
+    /// 提取子流域
+    /// 参数1 河流栅格
+    /// 参数2 河流方向
+    /// </summary>
+    public class Watersh
+    {
+        private string gpkFile;
+        public LocalGeoprocessingService gpService;
+        public GeoprocessingTask gpTask;
+        public GeoprocessingJob gpJob;
+
+        public Watersh(string gpkFile)
+        {
+            this.gpkFile = gpkFile;
+        }
+
+        private async Task initAsync()
+        {
+            if (LocalServerManager.localServer != null)
+            {
+                LocalServerManager.localServer.StatusChanged += async (o, e) =>
+                {
+                    if (e.Status != LocalServerStatus.Started)
+                    {
+                        throw new Exception("运行失败");
+                    }
+                    gpService = new LocalGeoprocessingService(gpkFile);
+                    gpService.StatusChanged += async (svc, args) =>
+                    {
+                        var gpSvcUrl = (svc as LocalGeoprocessingService).Url.AbsoluteUri + "\\模型.gpk";
+                        gpTask = new GeoprocessingTask(new Uri(gpSvcUrl));
+                        GeoprocessingParameters para = new GeoprocessingParameters(GeoprocessingExecutionType.SynchronousExecute);
+                        //输入两个参数
+                        string pathToRaster = @"c:\users\administrator\documents\arcgis\localServer\flowdir.tif";
+                        para.Inputs.Add("inputRaster1", new GeoprocessingRaster(new Uri(pathToRaster), ""));
+                        string pathToRaster2 = @"c:\users\administrator\documents\arcgis\localServer\river.tif";
+                        para.Inputs.Add("inputRaster2", new GeoprocessingRaster(new Uri(pathToRaster2), ""));
+                        para.ReturnZ = true;
+                        para.OutputSpatialReference = MainWindow.mainwindow.MyMapView.SpatialReference;
+                        gpJob = gpTask.CreateJob(para);
+
+
+
+                        //获取结果
+                        try
+                        {
+                            GeoprocessingResult geoprocessingResult = await gpJob.GetResultAsync();
+                            GeoprocessingRaster resultRaster = geoprocessingResult.Outputs["Watersh"] as GeoprocessingRaster;
+                            string Result = resultRaster.Source.AbsolutePath;
+                            var myRaster = new Esri.ArcGISRuntime.Rasters.Raster(Result);
+
+                            var newRasterLayer = new RasterLayer(myRaster);
+                            MainWindow.mainwindow.MyMapView.Map.OperationalLayers.Add(newRasterLayer);
+                            await MainWindow.mainwindow.MyMapView.SetViewpointGeometryAsync(newRasterLayer.FullExtent);
+
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+
+                  
+                };
+
+             
+                await gpService.StartAsync();
+            };
+                    
+            }
+        }
+
+    }
+}
---
+++ WpfApp1/form/SelectGP.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.Tasks.Geoprocessing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.form
+{
+    /// <summary>
+    /// SelectGP.xaml 的交互逻辑
+    /// </summary>
+    public partial class SelectGP : Window
+    {
+
+        private GeoprocessingTask _hotspotTask;
+        private GeoprocessingJob _hotspotJob;
+        private const string _hotspotUrl =
+            "https://www.arcgis.com/home/item.html?id=7c02e1c0427346c58e0544fabf816082";
+
+        public SelectGP()
+        {
+            InitializeComponent();
+            InitializeAsync();
+        }
+
+        private async Task InitializeAsync()
+        {
+            MyMapView.Map = new Map(Basemap.CreateTopographic());
+
+
+            try
+            {
+                // Create a new geoprocessing task
+                _hotspotTask = await GeoprocessingTask.CreateAsync(new Uri(_hotspotUrl));
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString()+"错误", "Error");
+            }
+
+
+            executeAsync();
+        }
+
+        private async Task executeAsync()
+        {
+            MyMapView.Map.OperationalLayers.Clear();
+            GeoprocessingParameters myHotspotParameters = new GeoprocessingParameters(GeoprocessingExecutionType.AsynchronousSubmit);
+            _hotspotJob = _hotspotTask.CreateJob(myHotspotParameters);
+            try
+            {
+                // Execute the geoprocessing analysis and wait for the results
+                GeoprocessingResult myAnalysisResult = await _hotspotJob.GetResultAsync();
+
+                // Add results to a map using map server from a geoprocessing task
+                // Load to get access to full extent
+                await myAnalysisResult.MapImageLayer.LoadAsync();
+
+                // Add the analysis layer to the map view
+                MyMapView.Map.OperationalLayers.Add(myAnalysisResult.MapImageLayer);
+
+                // Zoom to the results
+                await MyMapView.SetViewpointAsync(new Viewpoint(myAnalysisResult.MapImageLayer.FullExtent));
+            }
+            catch (TaskCanceledException)
+            {
+                // This is thrown if the task is canceled. Ignore.
+            }
+            catch (Exception ex)
+            {
+                // Display error messages if the geoprocessing task fails
+                if (_hotspotJob.Status == JobStatus.Failed && _hotspotJob.Error != null)
+                    MessageBox.Show("Executing geoprocessing failed. " + _hotspotJob.Error.Message, "Geoprocessing error");
+                else
+                    MessageBox.Show("An error occurred. " + ex, "Sample error");
+            }
+
+        }
+    }
+}
