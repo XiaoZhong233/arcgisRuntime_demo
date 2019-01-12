3fe34cc4713f95af3dbc6c59f60b07fe4b54c66d
钟浩伟 <610596547@qq.com>
2019-01-12 21:17:44 +0800

    加入属性表

---
+++ WpfApp1/ViewModel/DynamicColumn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.ViewModel
+{
+    public class DynamicColumn
+    {
+        public string Item_Name { get; set; }           //列名称
+        public List<string> Item_Value { get; set; }    //列值集合
+    }
+}
---
+++ WpfApp1/form/Controls/DataGrid.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.form.Controls
+{
+    /// <summary>
+    /// DataGrid.xaml 的交互逻辑
+    /// </summary>
+    public partial class DataGrid : UserControl
+    {
+
+        FeatureLayer layer;
+        IReadOnlyList<Field> fields;
+
+        public DataGrid(FeatureLayer layer)
+        {
+            InitializeComponent();
+            this.layer = layer;
+            init();
+        }
+
+        private void init()
+        {
+            fields = layer.FeatureTable.Fields;
+        }
+    }
+}
